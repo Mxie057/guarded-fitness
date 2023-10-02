6f5498de50bbe6f7f49f84ac18114af5e1e2742d
Michael <37895780+Mxie057@users.noreply.github.com>
2023-10-02 18:40:33 +1300

    Added logic to POST data to mongoDb

--- backend/server.js
+++ backend/server.js
@@ -23,14 +23,9 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for requests
         app.listen(process.env.PORT, () => {
-            console.log('Connected to DB and listening on port'. process.env.PORT)
+            console.log('Connected to DB and listening on port', process.env.PORT)
         })
     })
     .catch((error) => {
         console.log(error)
     })
-
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000');
-});
