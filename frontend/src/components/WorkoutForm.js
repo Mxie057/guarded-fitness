5eb8d19ee5aca6e61baf948a13edfe7e8a6ca723
Michael <37895780+Mxie057@users.noreply.github.com>
2023-10-03 00:28:08 +1300

    Finishing touches

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { set } from "mongoose";
 
 const WorkoutForm = () => {   
     const { dispatch } = useWorkoutsContext();
@@ -8,6 +9,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('');
     const [reps, setReps] = useState('');
     const [error, setError] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault() // Do not refresh page
@@ -26,12 +28,14 @@ const WorkoutForm = () => {
 
         if (!response.ok){
             setError(json.error);
+            setEmptyFields(json.emptyFields)
         }
         if(response.ok){
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('new workout added', json)
             dispatch({type: 'CREATE_WORKOUT', payload: json})
         }
@@ -44,18 +48,21 @@ const WorkoutForm = () => {
             <input type="text" 
             onChange={(e) => setTitle(e.target.value)}
             value={title}
+            className={emptyFields.includes('title') ? 'error' : ''}
             />
 
             <label>Load (in KG)</label>
             <input type="number" 
             onChange={(e) => setLoad(e.target.value)}
             value={load}
+            className={emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps</label>
             <input type="number" 
             onChange={(e) => setReps(e.target.value)}
             value={reps}
+            className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button>Add Workout</button>
