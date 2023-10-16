2bef46d5a5297a338ca2ce5bc3647e058512bf32
azharsadiq <azhar.sadiq@hotmail.com>
2023-10-16 20:59:38 +1300

    added login, signup and logout hooks

--- frontend/src/components/Navibar.js
+++ frontend/src/components/Navibar.js
@@ -2,11 +2,20 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 
 function Navibar() {
+  const { logout } = useLogout()
+  const { user } = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" bg="light">
       <Container>
         <img
           src="/img/logo.png"
@@ -24,14 +33,8 @@ function Navibar() {
               Workouts
             </Nav.Link>
           </Nav>
-          <Nav className="justify-content-end flex-grow-1 pe-3">
-            <Nav.Link as={Link} to="/signup">
-              Sign up
-            </Nav.Link>
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-          </Nav>
+          
+          
         </Navbar.Collapse>
       </Container>
     </Navbar>
